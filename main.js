f98cab11bdf817a3da2d7602ee8416ccb7b96abe
Liloganko <linwhat321485@gmail.com>
2025-08-20 00:13:12 +0800

    AOS

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// import 'aos/dist/aos.css';  // ← 這個一定要有
+// import 'aos/dist/aos.js';
+import AOS from 'aos';
 
+AOS.init({
+//   duration: 1000, // 動畫持續時間 (ms)
+  once:false,     // 是否只執行一次
+});
+
+
+// AOS.init();
 console.log('Hello world');
